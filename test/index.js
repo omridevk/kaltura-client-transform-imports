d2f7281d077080bd762399223a0138f44546940a
Omri Katz <omri.katz@kaltura.com>
2020-07-28 02:46:00 +0300

    delete lib folder

--- test/index.js
+++ test/index.js
@@ -2,3 +2,4 @@ import React, { Component } from "react";
 import {KalturaClient} from "kaltura-typescript-client";
 import {EntryVendorTaskExportToCsvAction, KalturaEntryVendorTaskFilter} from "kaltura-typescript-client/api/types";
 import { KalturaBaseEntry, KalturaESearchEntryResult } from "kaltura-typescript-client/api/types";
+import { KalturaReoccurringVendorCredit } from "kaltura-typescript-client/api/types";
