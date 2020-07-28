ed38393b06b91dfd0108622daef21e57575bb2bd
Omri Katz <omri.katz@kaltura.com>
2020-07-28 04:28:30 +0300

    add more imports to test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import {KalturaClient} from "kaltura-typescript-client";
-import {EntryVendorTaskExportToCsvAction, KalturaEntryVendorTaskFilter} from "kaltura-typescript-client/api/types";
+import { KalturaClient } from "kaltura-typescript-client";
+import {
+    EntryVendorTaskExportToCsvAction,
+    KalturaEntryVendorTaskFilter
+} from "kaltura-typescript-client/api/types";
+import { ReachProfileUpdateAction } from "kaltura-typescript-client/api/types/ReachProfileUpdateAction";
 import { KalturaBaseEntry, KalturaESearchEntryResult } from "kaltura-typescript-client/api/types";
+import { KalturaReachProfile } from "kaltura-typescript-client/api/types/KalturaReachProfile";
 import { KalturaReoccurringVendorCredit } from "kaltura-typescript-client/api/types";
